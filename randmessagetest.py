77cf15850b8d8273fd9f8f51ef06fd33917b5e8e
Nicholas Gingerella <nicholas.gingerella@gmail.com>
2015-05-20 22:43:02 -0700

    created script to test pycrypto usage with AES encryption and decryption

--- randmessagetest.py
+++ randmessagetest.py
@@ -1,8 +1,34 @@
+import os
 import string
 import random
+from Crypto.Cipher import AES
 
 #generate random string
 def randomMsg(size = 10, chars = string.ascii_letters + string.digits):
   return ''.join(random.choice(chars) for _ in range(size))
 
-print randomMsg(128)
+randomMsg = randomMsg(128)
+print randomMsg
+
+#NOTE: for AES, the password MUST be 16 bytes long, and the IV must be 16 bytes long
+
+aesKey = 'Sur9e 1s d b3ST!'
+aesIV = 'ouR 1v is super!'
+
+# Encryption
+encryption_suite = AES.new(aesKey, AES.MODE_CBC, aesIV)
+cipher_text = encryption_suite.encrypt(randomMsg)
+
+# Decryption
+decryption_suite = AES.new(aesKey, AES.MODE_CBC, aesIV)
+plain_text = decryption_suite.decrypt(cipher_text)
+
+print cipher_text
+print
+print plain_text
+
+if plain_text == randomMsg:
+  print 'Response to challenge successful, you are authorized'
+
+
+
